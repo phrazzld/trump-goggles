ec57c9036444b7f2af48d0788c69981b30ea503b
phrazzld <phrazzld@pm.me>
2025-05-03 22:29:02 +0900

    feat: add 2025 nickname upgrade pack
    - Add new RegEx patterns for current politicians and prosecutors
    - Update media organization nicknames
    - Add COVID-related terms
    - Improve pattern matching with case insensitivity
    - Organize patterns by category for better maintenance
    - Update Biden nickname from 'Crazy Joe Biden' to 'Sleepy Joe'
    - Update CNN nickname from 'Clinton News Network' to 'Fake News CNN'

--- content.js
+++ content.js
@@ -1,13 +1,11 @@
+// Original patterns
 var ISIS_PATTERN = new RegExp('(ISIS)|(ISIL)|(Islamic State)|(Isis)|(Isil)', 'g')
 var HILL_PATTERN = new RegExp('(Hillary Clinton)|(Hillary Rodham Clinton)|(Mrs\. Clinton)', 'g')
 var FAKENEWS_PATTERN = new RegExp('(MSNBC)|(NBC)|(NYTIMES)|(NYTimes)|(TNYT)|(NYT)|(New York Times)|(NBC News)|(HuffPo)|(Huffington Post)|(ABC News)|(American Broadcasting Company)|(CBS)', 'g')
-var CNN_PATTERN = new RegExp('CNN', 'g')
 var CRUZ_PATTERN = new RegExp('Ted Cruz', 'g')
 var MARCO_PATTERN = new RegExp('(Marco Rubio)|(Rubio)', 'g')
 var JEB_PATTERN = new RegExp('(Jeb Bush)|(Jeb)', 'g')
 var WARREN_PATTERN = new RegExp('Elizabeth Warren', 'g')
-var KIM_JONG_UN_PATTERN = new RegExp('(Kim Jong-un)|(Kim Jong Un)', 'g')
-var BIDEN_PATTERN = new RegExp('Joe Biden', 'g')
 var LAMB_PATTERN = new RegExp('Conor Lamb', 'g')
 var BANNON_PATTERN = new RegExp('Steve Bannon', 'g')
 var DURBIN_PATTERN = new RegExp('Dick Durbin', 'g')
@@ -20,11 +18,42 @@ var SCARBOROUGH_PATTERN = new RegExp('Joe Scarborough', 'g')
 var MIKA_PATTERN = new RegExp('Mika Brzezinski', 'g')
 var CHUCK_TODD_PATTERN = new RegExp('Chuck Todd', 'g')
 var JIM_ACOSTA_PATTERN = new RegExp('Jim Acosta', 'g')
-var CHUCK_SCHUMER_PATTERN = new RegExp('Chuck Schumer', 'g')
-var BERNIE_PATTERN = new RegExp('Bernie Sanders', 'g')
 var KASICH_PATTERN = new RegExp('John Kasich', 'g')
 var ASSAD_PATTERN = new RegExp('Bashar (Hafez)? al-Assad', 'g')
 var COFFEE_PATTERN = new RegExp('(coffee)|(Coffee)', 'g')
+
+// 2025 Upgrade Pack - Politicians & Prosecutors
+var BIDEN_PATTERN = new RegExp('Joe\\s+Biden', 'gi')
+var KAMALA_PATTERN = new RegExp('Kamala\\s+Harris', 'gi')
+var DESANTIS_PATTERN = new RegExp('Ron\\s+DeSantis', 'gi')
+var HALEY_PATTERN = new RegExp('Nikki\\s+Haley', 'gi')
+var MCCONNELL_PATTERN = new RegExp('Mitch\\s+McConnell', 'gi')
+var CHAO_PATTERN = new RegExp('Elaine\\s+Chao', 'gi')
+var SCHIFF_PATTERN = new RegExp('Adam\\s+Schiff', 'gi')
+var PELOSI_PATTERN = new RegExp('Nancy\\s+Pelosi', 'gi')
+var SCHUMER_PATTERN = new RegExp('Chuck\\s+Schumer', 'gi')
+var BLOOMBERG_PATTERN = new RegExp('(Michael|Mike)\\s+Bloomberg', 'gi')
+var CHENEY_PATTERN = new RegExp('Liz\\s+Cheney', 'gi')
+var CHRISTIE_PATTERN = new RegExp('Chris\\s+Christie', 'gi')
+var BERNIE_PATTERN = new RegExp('Bernie\\s+Sanders', 'gi')
+var JACK_SMITH_PATTERN = new RegExp('Jack\\s+Smith', 'gi')
+var BRAGG_PATTERN = new RegExp('Alvin\\s+Bragg', 'gi')
+var LETITIA_JAMES_PATTERN = new RegExp('Letitia\\s+James', 'gi')
+
+// Foreign Leaders
+var KIM_JONG_UN_PATTERN = new RegExp('(Kim Jong-un)|(Kim Jong Un)', 'gi')
+
+// Media & Organizations
+var CNN_PATTERN = new RegExp('CNN', 'gi')
+var NYT_PATTERN = new RegExp('(NYT|New\\s+York\\s+Times)', 'gi')
+var WP_PATTERN = new RegExp('(Washington\\s+Post|WaPo)', 'gi')
+var MSNBC_PATTERN = new RegExp('(MSNBC|MSDNC)', 'gi')
+var COMCAST_PATTERN = new RegExp('Comcast', 'gi')
+var FORBES_PATTERN = new RegExp('Forbes', 'gi')
+
+// COVID-related patterns
+var COVID_PATTERN = new RegExp('(COVID[- ]?19|Covid|Coronavirus)', 'gi')
+var COVID_ALT_PATTERN = new RegExp('(SARS[- ]CoV[- ]?2|Wuhan\\s+Virus)', 'gi')
 
 
 
@@ -65,18 +94,7 @@ function convert(textNode) {
 // Build RegEx patterns and replacements for each object of Trump's derision
 function buildTrumpMap () {
     return {
-        "cnn": {
-            "regex": CNN_PATTERN,
-            "nick": "Clinton News Network"
-        },
-        "kasich": {
-            "regex": KASICH_PATTERN,
-            "nick": "1 for 38 Kasich"
-        },
-        "bernie": {
-            "regex": BERNIE_PATTERN,
-            "nick": "Crazy Bernie"
-        },
+        // Original mappings
         "isis": {
             "regex": ISIS_PATTERN,
             "nick": "Evil Losers"
@@ -104,14 +122,6 @@ function buildTrumpMap () {
         "warren": {
             "regex": WARREN_PATTERN,
             "nick": "Goofy Pocahontas"
-        },
-        "kimjongun": {
-            "regex": KIM_JONG_UN_PATTERN,
-            "nick": "Little Rocket Man"
-        },
-        "biden": {
-            "regex": BIDEN_PATTERN,
-            "nick": "Crazy Joe Biden"
         },
         "lamb": {
             "regex": LAMB_PATTERN,
@@ -161,9 +171,9 @@ function buildTrumpMap () {
             "regex": JIM_ACOSTA_PATTERN,
             "nick": "Crazy Jim Acosta"
         },
-        "chuckschumer": {
-            "regex": CHUCK_SCHUMER_PATTERN,
-            "nick": "Cryin' Chuck Schumer"
+        "kasich": {
+            "regex": KASICH_PATTERN,
+            "nick": "1 for 38 Kasich"
         },
         "coffee": {
             "regex": COFFEE_PATTERN,
@@ -172,6 +182,112 @@ function buildTrumpMap () {
         "assad": {
             "regex": ASSAD_PATTERN,
             "nick": "Animal Assad"
+        },
+        
+        // 2025 Upgrade Pack - Politicians & Prosecutors
+        "biden": {
+            "regex": BIDEN_PATTERN,
+            "nick": "Sleepy Joe"
+        },
+        "kamala": {
+            "regex": KAMALA_PATTERN,
+            "nick": "Laffin' Kamala"
+        },
+        "desantis": {
+            "regex": DESANTIS_PATTERN,
+            "nick": "Ron DeSanctimonious"
+        },
+        "haley": {
+            "regex": HALEY_PATTERN,
+            "nick": "Birdbrain Nikki"
+        },
+        "mcconnell": {
+            "regex": MCCONNELL_PATTERN,
+            "nick": "Old Crow Mitch"
+        },
+        "chao": {
+            "regex": CHAO_PATTERN,
+            "nick": "Coco Chow"
+        },
+        "schiff": {
+            "regex": SCHIFF_PATTERN,
+            "nick": "Shifty Schiff"
+        },
+        "pelosi": {
+            "regex": PELOSI_PATTERN,
+            "nick": "Crazy Nancy"
+        },
+        "schumer": {
+            "regex": SCHUMER_PATTERN,
+            "nick": "Cryin' Chuck"
+        },
+        "bloomberg": {
+            "regex": BLOOMBERG_PATTERN,
+            "nick": "Mini Mike"
+        },
+        "cheney": {
+            "regex": CHENEY_PATTERN,
+            "nick": "Lyin' Liz"
+        },
+        "christie": {
+            "regex": CHRISTIE_PATTERN,
+            "nick": "Sloppy Chris"
+        },
+        "bernie": {
+            "regex": BERNIE_PATTERN,
+            "nick": "Crazy Bernie"
+        },
+        "jacksmith": {
+            "regex": JACK_SMITH_PATTERN,
+            "nick": "Deranged Jack Smith"
+        },
+        "bragg": {
+            "regex": BRAGG_PATTERN,
+            "nick": "Fat Alvin"
+        },
+        "letitiajames": {
+            "regex": LETITIA_JAMES_PATTERN,
+            "nick": "Peekaboo"
+        },
+        "kimjongun": {
+            "regex": KIM_JONG_UN_PATTERN,
+            "nick": "Little Rocket Man"
+        },
+        
+        // Media & Organizations
+        "cnn": {
+            "regex": CNN_PATTERN,
+            "nick": "Fake News CNN"
+        },
+        "nyt": {
+            "regex": NYT_PATTERN,
+            "nick": "Failing New York Times"
+        },
+        "washingtonpost": {
+            "regex": WP_PATTERN,
+            "nick": "Amazon Washington Post"
+        },
+        "msnbc": {
+            "regex": MSNBC_PATTERN,
+            "nick": "MSDNC"
+        },
+        "comcast": {
+            "regex": COMCAST_PATTERN,
+            "nick": "Concast"
+        },
+        "forbes": {
+            "regex": FORBES_PATTERN,
+            "nick": "Failing Forbes Magazine"
+        },
+        
+        // COVID-related terms
+        "covid": {
+            "regex": COVID_PATTERN,
+            "nick": "China Virus"
+        },
+        "covidalt": {
+            "regex": COVID_ALT_PATTERN,
+            "nick": "Kung Flu"
         }
     }
 }
